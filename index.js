f8d8b5cabe6085a5556cfdc02d3c9971ec257fcd
ReggeMeister <mattiaswaern04@gmail.com>
2026-03-05 23:35:40 +0100

    commit

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
-import readlinkSync from "readline-sync";
+import readlineSync from "readline-sync";
 import fs from "fs/promises";
+import { appName } from "./modul1.js";
+import { getGreeting } from "./modul2.js";
 
+const text = readlineSync.question("Skriv en sak som ska sparas: ");
 
-const text = readlinkSync.question("Skriv en sak som ska sparas: ");
+async function main() {
+  try {
 
+    await fs.writeFile("data.txt", text, "utf-8");
+    console.log("File written successfully");
 
-async function main(){
-    try{
-        await fs.writeFile("data.txt", text);
-        console.log("File written successfully");
+    const data = await fs.readFile("data.txt", "utf-8");
+    console.log(data);
 
+    const html = `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>${appName}</title>
+</head>
 
-        const data = await fs.readFile("data.txt", "utf-8");
-        console.log(data);
-    } 
-    catch (error){
-        console.error("An error occurred while reading the file:", error);
-} 
+<body>
+<h1>${getGreeting()} ${appName}</h1>
+</body>
+</html>`;
+
+    await fs.writeFile("index.html", html, "utf-8");
+    console.log("index.html created");
+
+  } catch (error) {
+    console.error("An error occurred:", error);
+  }
 }
 
 main();
