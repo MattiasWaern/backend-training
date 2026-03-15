b566dbd6941175a85a71cd593b7dc5c81f7f13e8
ReggeMeister <mattiaswaern04@gmail.com>
2026-03-15 11:06:02 +0100

    Update server.js

--- rest API/server.js
+++ rest API/server.js
@@ -10,5 +10,6 @@ app.use(express.json());
 let users = [
    { id: 1, name: 'Alice', email: 'alicegamer@example.com'},
    { id: 2, name: 'Bob', email: 'bobgamer@example.com'},
-   { id: 3, name: 'Charlie', email: 'charliegamer@example.com'}
+   { id: 3, name: 'Charlie', email: 'charliegamer@example.com'},
+   { id: 4, name: 'Annika', email: 'annikagamer@example.com'}
 ]
