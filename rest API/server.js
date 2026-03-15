982ef41474f1d00285aa4c66f1817853b2bcd96d
ReggeMeister <mattiaswaern04@gmail.com>
2026-03-15 11:06:20 +0100

    Update server.js

--- rest API/server.js
+++ rest API/server.js
@@ -6,10 +6,11 @@ const PORT = 3000;
 app.use(express.json());
 
 //mockdata
-
 let users = [
    { id: 1, name: 'Alice', email: 'alicegamer@example.com'},
    { id: 2, name: 'Bob', email: 'bobgamer@example.com'},
    { id: 3, name: 'Charlie', email: 'charliegamer@example.com'},
    { id: 4, name: 'Annika', email: 'annikagamer@example.com'}
 ]
+
+//GET all users
