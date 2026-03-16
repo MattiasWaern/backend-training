1d8073cfe06d0fd1a77016c9f4fa278d77075118
ReggeMeister <mattiaswaern04@gmail.com>
2026-03-16 14:16:58 +0100

    commit

--- server.js
+++ server.js
@@ -14,3 +14,37 @@ let users = [
 ]
 
 //GET all users
+app.get('/users', (req, res) => {
+   res.json(users);
+})
+
+//GET user by id
+app.get('/users/:id', (req, res) => {
+   const userId = parseInt(req.params.id);
+   const user = users.find(u => u.d === userId);
+   if(user){
+      res.json(user);
+   } else {
+      res.status(404).json({error: 'User not found'});
+   }
+})
+
+//POST create new user
+app.post('/users', (req, res) => {
+   const {name, eemail} = req.body;
+   if(name && email){
+      const newUser = {
+         id: users.length + 1,
+         name,
+         email
+      }
+      users.push(newUser);
+      res.status(201).json(newUser);
+   } else {
+      res.status(400).json({error: 'Invalid user data'});
+   }
+})
+
+app.listen(PORT, () => {
+   console.log(`Server is running on http://localhost:${PORT}`);
+})
